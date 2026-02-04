bd646e6f228b50816efb601cd65e0ea540925ab2
Madis Pukkonen <mpukkonen@gmail.com>
2026-02-04 14:44:37 +0200

    fix: cleanup

--- src/_data/posthog.js
+++ src/_data/posthog.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiKey: "phc_yQhy5MsO2GL4VLTaGrNjthV15fDXXQOD4gkMBPatFzV", // Replace with your PostHog project API key
-  apiHost: "https://us.i.posthog.com", // Use https://us.i.posthog.com for US cloud
+  apiKey: "phc_yQhy5MsO2GL4VLTaGrNjthV15fDXXQOD4gkMBPatFzV",
+  apiHost: "https://us.i.posthog.com",
 };
